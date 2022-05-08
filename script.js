e1a3306030e96fcea60c4e36030b5e2041a2a023
Grant Littlejohn <glittlejohn46@yahoo.com>
2022-05-07 17:43:39 -0700

    add calculation
    create functions for calculations with operators

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const percentage = document.querySelector(".percent");
 const numberButtons = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const decimal = document.querySelector(".decimal");
-const equal = document.querySelector(".equals");
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", calc);
 
 numberButtons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -22,4 +23,35 @@ numberButtons.forEach((btn) => {
 function handleNumber(num) {
     currentNum += num;
     currentDisplayNum.textContent = currentNum;
+}
+
+operators.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        handleOperator(e.target.textContent);
+    })
+});
+
+function handleOperator(op) {
+    operator = op;
+    prevNum = currentNum;
+    prevDisplayNum.textContent = prevNum + " " + operator;
+    currentNum ="";
+    currentDisplayNum.textContent = "";
+}
+
+function calc() {
+    prevNum = Number(prevNum);
+    currentNum = Number(currentNum);
+
+    if(operator === "+") {
+        prevNum = prevNum + currentNum;
+    } else if(operator === "-") {
+        prevNum = prevNum - currentNum;
+    } else if(operator === "X") {
+        prevNum = prevNum * currentNum;
+    } else if(operator === "÷") {
+        prevNum = prevNum / currentNum;
+    }
+    prevDisplayNum.textContent = "";
+    currentDisplayNum.textContent = prevNum;
 }
